8324717687cb5494b1e8bdfa1a623803337969af
Victor Alcaraz <vctralcaraz@gmail.com>
2024-06-03 23:57:26 -0700

    email verification in htmx and go

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -23,10 +23,67 @@ func newTemplate() *Templates {
 	}
 }
 
+type Contact struct {
+	Name, Email string
+}
+
+func newContact(name, email string) Contact {
+	return Contact{
+		Name:  name,
+		Email: email,
+	}
+}
+
+type Contacts = []Contact
+
+type Data struct {
+	Contacts Contacts
+}
+
+func (d *Data) hasEmail(email string) bool {
+	for _, contact := range d.Contacts {
+		if contact.Email == email {
+			return true
+		}
+	}
+	return false
+}
+
+func newData() Data {
+	return Data{
+		Contacts: []Contact{
+			newContact("John", "jd@gmail.com"),
+			newContact("Clara", "cd@gmail.com"),
+		},
+	}
+}
+
 type Count struct {
 	Count int
 }
 
+type FormData struct {
+	Values, Errors map[string]string
+}
+
+func newFormData() FormData {
+	return FormData{
+		Values: make(map[string]string),
+		Errors: make(map[string]string),
+	}
+}
+
+type Page struct {
+	Data Data
+	Form FormData
+}
+
+func newPage() Page {
+	return Page{
+		Data: newData(),
+		Form: newFormData(),
+	}
+}
 func main() {
 	// var text string = "duh"
 	// fmt.Printf("This is a test of my go code. %s\n", text)
@@ -37,16 +94,29 @@ func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
 
-	count := Count{Count: 0}
+	page := newPage()
 	e.Renderer = newTemplate()
 
 	e.GET("/", func(c echo.Context) error {
-		return c.Render(200, "index", count)
+		return c.Render(200, "index", page)
 	})
 
-	e.POST("/count", func(c echo.Context) error {
-		count.Count++
-		return c.Render(200, "index", count)
+	e.POST("/contacts", func(c echo.Context) error {
+		name := c.FormValue("name")
+		email := c.FormValue("email")
+
+		if page.Data.hasEmail(email) {
+			formData := newFormData()
+			formData.Values["name"] = name
+			formData.Values["email"] = email
+			formData.Errors["email"] = "Email already exists"
+
+			return c.Render(400, "form", formData)
+		}
+
+		page.Data.Contacts = append(page.Data.Contacts, newContact(name, email))
+
+		return c.Render(200, "display", page)
 	})
 
 	e.Logger.Fatal(e.Start(":42069"))
